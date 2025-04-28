0ebd51f56299798826ce41e05236b9ccbb89d31d
julesfakhouri1 <julesfakhouri75@ecomail.fr>
2025-04-28 14:08:08 +0200

    correction

--- test_model.py
+++ test_model.py
@@ -4,7 +4,7 @@ def test_predict_positive():
     assert predict_sentiment("I am happy today") == "positive"
 
 def test_predict_negative():
-    assert predict_sentiment("I feel sad") == "positif"
+    assert predict_sentiment("I feel sad") == "negative"
 
 def test_predict_neutral():
     assert predict_sentiment("The sky is blue") == "neutral"
